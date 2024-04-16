498e70a93a29675f05632218bd089ed7809ffab2
Denis <mugambimuthuri0@gmail.com>
2024-04-16 05:52:15 +0300

    Appending card body to div

--- js/index.js
+++ js/index.js
@@ -18,11 +18,36 @@ function fetchLocation() {
 
   })
     .then(res => res.json())
-    .then(location => {
-      console.log(location)
+    .then(locations => {
+      locations.forEach((location)=> renderLocation(location))
     }).catch(
       (error) => {
         console.log(error)
       }
     )
+}
+function renderLocation(location) {
+  const locationsContainer = document.querySelector('#locations');
+
+  const parentDiv =document.createElement('div');
+  parentDiv.classList.add('card')
+  const image =document.createElement('img');
+  image.classList.add('card-img-top','mt-2');
+  image.height = 200;
+  image.src = location.Thumb;
+  image.alt = location.Name;
+  //append the image to the div
+  parentDiv.appendChild(image);
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  //location name
+  const name = document.createElement('h5')
+  name.className.add('card-title');
+  name.innerText = location.Name;
+
+  cardBody.append(name);
+  //append cardbody to parent div
+
+  //append each card to the location container
+  locationsContainer.appendChild(parentDiv);
 }
