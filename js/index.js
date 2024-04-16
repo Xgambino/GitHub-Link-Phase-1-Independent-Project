382d130231d1b972b034862365356ae28c68d2b4
Denis <mugambimuthuri0@gmail.com>
2024-04-16 07:41:42 +0300

    Clicks Notification

--- js/index.js
+++ js/index.js
@@ -1,60 +1,77 @@
-const BASE_URL = 'http://localhost:3000/location';
-
+const BASE_URL = "http://localhost:3000/location";
 
 // FETCH LOCATIONS
 
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
   fetchLocation();
+  const form = document.querySelector("#search-form");
+  form.addEventListener("submit", (e) => {
+    //prevent default behavior
+    e.preventDefault();
+    const input = document.querySelector("#search");
+    if (fetchLocation(input.value));
+  });
 });
 
-function fetchLocation() {
-
-  fetch(`${ BASE_URL }`, {
-    method: 'GET',
+function fetchLocation(searchTerm = "") {
+  fetch(`${BASE_URL}`, {
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json'
-    }
-
+      "Content-Type": "application/json",
+    },
   })
-    .then(res => res.json())
-    .then(locations => {
-      locations.forEach((location)=> renderLocation(location))
-    }).catch(
-      (error) => {
-        console.log(error)
+    .then((res) => res.json())
+    .then((locations) => {
+      //if search term is available we filter
+      if (searchTerm) {
+        //clear locations
+        document.querySelector("locations").innerHTML = "";
+        //filter out locztions that match the search input
+        locations
+          .filter((location) =>
+            location.name.toLowerCase().includes(searchTerm)
+          )
+          .forEach((location) => renderLocation(location));
+      } else {
+        locations.forEach((location) => renderLocation(location));
       }
-    )
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 function renderLocation(location) {
-  const locationsContainer = document.querySelector('#locations');
+  const locationsContainer = document.querySelector("#locations");
 
-  const parentDiv =document.createElement('div');
-  parentDiv.classList.add('card')
-  const image =document.createElement('img');
-  image.classList.add('card-img-top','mt-2');
+  const parentDiv = document.createElement("div");
+  parentDiv.classList.add("card");
+  const image = document.createElement("img");
+  image.classList.add("card-img-top", "mt-2");
   image.height = 200;
   image.src = location.Thumb;
   image.alt = location.Name;
   //append the image to the div
   parentDiv.appendChild(image);
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
 
   //location name
-  const name = document.createElement('h5')
-  name.className = ('card-title');
+  const name = document.createElement("h5");
+  name.className = "card-title";
   name.innerText = location.Name;
 
   //location description
-  const description= document.createElement('p');
-  description.className = ('card-text');
+  const description = document.createElement("p");
+  description.className = "card-text";
   description.innerText = location.Description;
 
   //button
-  const button = document.createElement('button');
-  button.classList.add('btn','btn-primary');
-  button.innerText = 'Book With This Location';
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-primary");
+  button.innerText = "Book With This Location";
+  button.addEventListener('click', () => {
+    alert(`Location with id ${location.id} clicked`);
+  })
 
   //append description to cardbody
 
